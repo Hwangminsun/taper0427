17ade51aa3696295ab44f8145a2cae5677c5f14e
Hwangminsun <dai01765628@gmail.com>
2023-06-27 09:39:00 +0900

    Add files via upload

--- php/member_form.php
+++ php/member_form.php
@@ -4,14 +4,21 @@
 
 <!doctype html>
 <html lang="ko">
-	<head>
-		<meta charset="utf-8">
-		<title>JOIN US | 회원가입</title>
-		<link rel="stylesheet" href="../css/reset.css">
-		<link rel="stylesheet" href="../css/base.css">
-		<link rel="stylesheet" href="../css/member_form.css">
-		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TAPERED COFFEE</title>
+	<link rel="stylesheet" href="../css/reset.css">
+	<!-- <link rel="stylesheet" href="../css/base.css"> -->
+	<link rel="stylesheet" href="../css/member_form.css">
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
 		
+	<script src="../js/jquery-1.11.2.min.js"></script>
+  	<script src="../js/menu.js"></script>
+	  <script src="https://kit.fontawesome.com/dbb7afa741.js" crossorigin="anonymous"></script>
+  	<script src="../js/bottomnav.js"></script>
+
 <script>
 //let reg_pw2 = /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{6,24}/; // 문자와 특수문자 조합의 6~24 자리
 // const re1 = /^[a-zA-Z0-9]{4,12}$/ ;  //가능하다
@@ -127,12 +134,64 @@ function reset_form() {
 	document.member_form.id.focus();
 	return;
 }
-</script>
-		
+</script>		
 </head>
+
 <body>
-	
-	<h1>JOIN US &nbsp;| &nbsp;회원가입</h1>
+<div id="skip-menu">
+    <a href="#menu_wrap">&#187; 메뉴 바로가기</a>
+    <a href="#group">&#187; 내용 바로가기</a>
+  </div>
+
+  <!-- header -->
+
+  <header class="cf">
+    <!-- 모바일 햄버거 버튼 -->
+    <div id="ham-wrap">
+      <div class="hamburger">
+         <span></span>  <span></span> <span></span> 
+      </div> 
+  </div>
+
+    <h1><a href="../index.html"><img src="../img/taper-logo.png" alt="logo"></a></h1>
+    <!-- 태블릿, pc 버전 사용 -->
+    <ul class="right">
+        <li><a href="#" onclick="return false;"><i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
+        <li><a href="#" onclick="return false;">Login</a></li>
+        <li><a href="javascript:void(0)"><i class="fa-solid fa-file-pen"></i></li>
+        <li><a href="#contact">Service</a></li>
+        <li><a href="javascript:void(0)"><i class="fa-solid fa-map"></i></a></li>
+        <li><a href="#cafe_resize">Location</a></li>
+    </ul>
+  </header>
+
+  <!-- 모바일 버전 사용 -->
+  <div class="bottomNav" id="usp_sell">
+    <ul>
+      <li><a href="#" class="current"><i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
+      <li><a class="map" href="#cafeloca"><i class="fa-solid fa-map"></i></a></li>
+      <li><a href="#contact"><i class="fa-solid fa-file-pen"></i></a></li>
+      <li><a href="javascript:void(0)">Login</a></li>
+      <li><a href="javascript:void(0)">Location</a></li>
+      <li><a href="javascript:void(0)">Service</a></li>
+    </ul>
+  </div>
+
+  <nav id="menu-wrap"> <!--메뉴 전체-->
+      <ul class="menu">
+        <li><a href="#" onclick="return false;">Menu</a></li>
+        <li><a href="#" onclick="return false;">Store</a></li>
+        <li><a href="#" onclick="return false;">Brand</a></li>
+      </ul>   
+  </nav>
+
+  <aside>
+    <div class="go-top"></div>
+  </aside>
+  <!-- header 끝 -->
+
+<section>
+	<h2>JOIN US &nbsp;| &nbsp;회원가입</h1>
 	
 	<div id="group">
 		<div class="close">
@@ -172,5 +231,12 @@ function reset_form() {
 			</div>
 		</form>
 	</div>
+</section>
+
+<footer>
+  <div id="footer"><a href="https://www.instagram.com/taperedcoffee/"
+    target="_blank">photo by TAPERED COFFEE INSTAGRAM</a></div>
+</footer>
+
 </body>
 </html>
